df8f463784d089f1f8ac23b446e8e612087184ca
maniac <worldcerberus.com>
2022-11-23 18:39:20 +0530

    my protfolio

---
+++ src/content_option.js
@@ -0,0 +1,164 @@
+const logotext = "Aman Faridi";
+const meta = {
+    title: "Aman Faridi",
+    description: "I’m Aman Faridi blockchain devloper _ Full stack devloper,currently working in Web3Dao ",
+};
+
+const introdata = {
+    title: "I’m Aman Faridi",
+    animated: {
+        first: "I love coding",
+        second: "I code cool smart contracts",
+        third: "I develop Crypto currencies , NFT's",
+    },
+    description: "I am a highly skilled individual who can efficiently code for designing, developing and testing blockchain applications.",
+    your_img_url: "https://live.staticflickr.com/65535/52514232196_a818498d72_b.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "A skilled blockchain engineer with more than 1 years of professional experience looking to contribute knowledge in web and blockchain technology to  Web3Dao. Proficient at performing well as working in a group or independently in a fast-paced setting. Deep understanding of distributed ledger, blockchain safety, blockchain design principles, blockchain smart contracts, and various blockchain protocols and environments.",
+};
+const worktimeline = [{
+        jobtitle: "Real World Projects",
+        where: "Remote",
+        date: "2022",
+    },
+    {
+        jobtitle: "Deep understanding in blockchain",
+        where: "Remote",
+        date: "2020",
+    },
+];
+
+const skills = [{
+        name: "Solidity",
+        value: 90,
+    },
+    {
+        name: "JavaScript frameworks ( React , Next )",
+        value: 85,
+    },
+    {
+        name: "EtherJs",
+        value: 80,
+    },
+    {
+        name: "Contracts( ERC20 , ERC721 , ERC1155)",
+        value: 83,
+    },
+    {
+        name: "Hand on Tools ( Hardhat , Truffle , Remix)",
+        value: 90,
+    },
+        {
+        name: "Excelent Knowledge of Openzeppelin",
+        value: 80,
+    },
+
+];
+
+const services = [{
+        title: "Dapps",
+        description: "Using the solidity framework features, optimized the smart contract program and boosted performance by 20%",
+    },
+    {
+        title: "Defi",
+        description: "This individual has successfully delivered products in DeFi, NFT, smart contract, and wallets",
+    },
+    {
+        title: "NFT",
+        description: "NFT developer with professional skills. And good understanding what a non-fungible token is and how different types of projects are developed using NFTs",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://live.staticflickr.com/65535/52513830152_004149cf79_b.jpg",
+        desctiption: "Launched my NFT collection named Crypto Devs. I want to give early supporters access to a whitelist for my collection, so here i have created a whitelist dapp for Crypto Devs",
+        link: "https://myapps-ten.vercel.app/",
+    },
+    {
+        img: "https://live.staticflickr.com/65535/52514583144_e2d7e374f3_b.jpg",
+        desctiption: "Launched my own Aman's NFT collection",
+        link: "https://aman-nft-collection.vercel.app/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "saphyra_d@protonmail.ch",
+    YOUR_FONE: "+91-7461820305 , +91-6207711472",
+    description: `I'll get back to you soon , Please leave the massage here !`,
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_s6w0ulc",
+    YOUR_TEMPLATE_ID: "template_d21834c",
+    YOUR_USER_ID: "vEIJHRB8CZC6KFVh0",
+};
+
+const socialprofils = {
+    github: "https://github.com/kronozz1",
+    twitter: "https://twitter.com/Kronoz_lsd",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
